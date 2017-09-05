2efb5c4e190ed2dd1d8b94783d7ae872a6dbf1de
Yauheni <slesh.eugene93@gmail.com>
2017-09-05 18:43:28 +0300

    the weather actor was fixed

--- api/app/actors/WeatherActor.scala
+++ api/app/actors/WeatherActor.scala
@@ -1,11 +1,11 @@
 package actors
 
-//import akka.actor._
+import akka.actor._
 
-class WeatherActor /*extends Actor*/ {
+class WeatherActor extends Actor {
 
-//  def receive(message: WeatherMessage): Actor.Receive = {
-//    case (city) => //a http call to the weather api
-//  }
+  override def receive: Actor.Receive = {
+    case WeatherMessage(city) => ???
+  }
 
 }
